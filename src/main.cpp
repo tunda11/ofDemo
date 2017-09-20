37b84196337a2a89e5ca55d640b9649fdbe16c62
tunda11 <tunda11@users.noreply.github.com>
2017-09-20 15:52:11 -0400

    ofApp.h

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
 
 //========================================================================
 int main( ){
+	cout << "Hello from main" << endl;
 	ofSetupOpenGL(1024,768,OF_WINDOW);			// <-------- setup the GL context
 
 	// this kicks off the running of my app
